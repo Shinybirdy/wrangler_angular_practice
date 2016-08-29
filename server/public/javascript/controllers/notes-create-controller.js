e4b66f902e64f29567d71cb8591433d41c488a65
Tracey Zavadil <shinybirdy@yahoo.com>
2016-08-28 23:49:20 -0500

    ugh

--- server/public/javascript/controllers/notes-create-controller.js
+++ server/public/javascript/controllers/notes-create-controller.js
@@ -9,3 +9,4 @@ angular.module('NoteWrangler')
     });
   };
 });
+// what the?
--- server/public/javascript/controllers/notes-edit-controller.js
+++ server/public/javascript/controllers/notes-edit-controller.js
@@ -1,7 +1,7 @@
 angular.module("NoteWrangler")
 .controller("NotesEdit Controller", function($scope, $http){
   $scope.updateNote = function(noteObj){
-    $http({method: "PUT", url: "/notes", data: noteObj})
-  }
-})
+    $http({method: "PUT", url: "/notes", data: noteObj});
+  };
+});
 // Services should hold functions responsible for connecting and fetching data, and then sharing it across our application
